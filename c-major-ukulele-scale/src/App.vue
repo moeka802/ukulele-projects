29c56061bd547dfbe1829fac8c82885f7de003ba
moeka802 <42276835+moeka802@users.noreply.github.com>
2023-04-14 08:40:21 +0900

    C Majorスケールを作成

---
+++ c-major-ukulele-scale/src/App.vue
@@ -0,0 +1,141 @@
+<template>
+  <div class="container">
+    <h1 class="heroTitle">C Major Ukulele Scale</h1>
+    <div class="fretboardContainer">
+      <fret v-for="(note, i) in notes" :key="i" :note="note"></fret>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import Fret from "./components/FretComponent.vue";
+const NOTES = [
+  { name: "A" },
+  { name: "B♭" },
+  { name: "B" },
+  { name: "C" },
+  { name: "D♭" },
+  { name: "D" },
+  { name: "E♭" },
+  { name: "E" },
+  { name: "F" },
+  { name: "G♭" },
+  { name: "G" },
+  { name: "A♭" },
+  { name: "A" },
+  { name: "B♭" },
+  { name: "B" },
+
+  { name: "E" },
+  { name: "F" },
+  { name: "G♭" },
+  { name: "G" },
+  { name: "A♭" },
+  { name: "A" },
+  { name: "B♭" },
+  { name: "B" },
+  { name: "C" },
+  { name: "D♭" },
+  { name: "D" },
+  { name: "E♭" },
+  { name: "E" },
+  { name: "F" },
+  { name: "G♭" },
+
+  { name: "C" },
+  { name: "D♭" },
+  { name: "D" },
+  { name: "E♭" },
+  { name: "E" },
+  { name: "F" },
+  { name: "G♭" },
+  { name: "G" },
+  { name: "A♭" },
+  { name: "A" },
+  { name: "B♭" },
+  { name: "B" },
+  { name: "C" },
+  { name: "D♭" },
+  { name: "D" },
+
+  { name: "G" },
+  { name: "A♭" },
+  { name: "A" },
+  { name: "B♭" },
+  { name: "B" },
+  { name: "C" },
+  { name: "D♭" },
+  { name: "D" },
+  { name: "E♭" },
+  { name: "E" },
+  { name: "F" },
+  { name: "G♭" },
+  { name: "G" },
+  { name: "A♭" },
+  { name: "A" },
+];
+
+export default defineComponent({
+  name: "App",
+  components: {
+    Fret,
+  },
+  data() {
+    return {
+      isReady: false,
+      notes: [] as any,
+    };
+  },
+  mounted() {
+    this.notes = NOTES.map((note) => {
+      if (["C", "D", "E", "F", "G", "A", "B"].includes(note.name)) {
+        return {
+          ...note,
+          isVisible: true,
+          isRoot: note.name === "C",
+        };
+      } else {
+        return {
+          name: "",
+          isVisible: false,
+          isRoot: false,
+        };
+      }
+    });
+  },
+});
+</script>
+
+<style>
+body {
+  margin: 0px;
+  padding: 0px;
+  box-sizing: border-box;
+}
+
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+.container {
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+
+h1 {
+  margin-bottom: 50px;
+}
+
+.fretboardContainer {
+  display: grid;
+  grid-template-columns: repeat(15, 1fr);
+}
+</style>
---
+++ c-major-ukulele-scale/src/components/FretComponent.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="fret">
+    <div v-if="note.isVisible" class="fretFinger">{{ note.name }}</div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  props: {
+    note: {
+      type: Object,
+    },
+  },
+  computed: {
+    theme() {
+      return this.note.isRoot
+        ? { color: "#fff", backgroundColor: "#ff7f50" }
+        : { color: "#999", backgroundColor: "#eee" };
+    },
+  },
+});
+</script>
+
+<style>
+.fret {
+  position: relative;
+  width: 60px;
+  height: 60px;
+  border-top: 1px solid #2c3e50;
+  border-left: 1px solid #2c3e50;
+  z-index: 10;
+}
+
+.fretFinger {
+  position: absolute;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  z-index: 20;
+
+  background: v-bind("theme.backgroundColor");
+  color: v-bind("theme.color");
+  font-weight: bold;
+
+  width: 40px;
+  height: 40px;
+  border-radius: 50%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.fret:nth-of-type(15n + 1) {
+  border-left: 0px;
+}
+.fret:nth-of-type(15n + 2) {
+  border-left-style: dotted;
+}
+.fret:nth-of-type(n + 46) {
+  border-left: 0px;
+}
+</style>
